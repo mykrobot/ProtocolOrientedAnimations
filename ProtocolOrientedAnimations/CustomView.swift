ffcf619643dd1c276da823d73c94ca86e6523067
Michael Mecham <myke.mecham@gmail.com>
2016-09-04 01:23:09 -0600

    Demo Project Updated.

---
+++ ProtocolOrientedAnimations/CustomView.swift
@@ -0,0 +1,11 @@
+//
+//  CustomView.swift
+//  ProtocolOrientedAnimations
+//
+//  Created by Michael Mecham on 9/2/16.
+//  Copyright © 2016 MichaelMecham. All rights reserved.
+//
+
+import UIKit
+
+class CustomView: UIView, Moveable {}
--- ProtocolOrientedAnimations/DemoViewController.swift
+++ ProtocolOrientedAnimations/DemoViewController.swift
@@ -12,6 +12,53 @@ class DemoViewController: UIViewController {
 
     @IBOutlet weak private var demoTextField: CustomTextField!
     
+    @IBOutlet weak private var firstView: CustomView!
+    @IBOutlet weak private var secondView: CustomView!
+    @IBOutlet weak private var thirdView: CustomView!
+    @IBOutlet weak private var plusXButton: UIButton!
+    
+    private var subMenuShowing = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        roundCornersOnSubMenu()
+    }
+    
+    private func roundCornersOnSubMenu() {
+        let views = [firstView, secondView, thirdView]
+        for view in views {
+            view.layer.cornerRadius = view.frame.width / 2
+            view.layer.masksToBounds = true
+        }
+    }
+    
+    @IBAction func toggleSubMenu(sender: AnyObject) {
+        if subMenuShowing {
+            hideSubMenu()
+        } else {
+            showSubMenu()
+        }
+        subMenuShowing = !subMenuShowing
+    }
+    
+    private func hideSubMenu() {
+        UIView.animateWithDuration(0.4) { 
+            self.plusXButton.transform = CGAffineTransformMakeRotation(CGFloat(M_PI))
+        }
+        secondView.move(.Up, points: 58)
+        firstView.move(.Up, points: 116)
+        
+    }
+    
+    private func showSubMenu() {
+        UIView.animateWithDuration(0.4) { 
+            self.plusXButton.transform = CGAffineTransformMakeRotation(CGFloat(-M_PI_4))
+        }
+        secondView.move(.Down, points: 58)
+        firstView.move(.Down, points: 116)
+        
+    }
+    
     @IBAction func shakeButtonTapped(sender: AnyObject) {
         demoTextField.shake()
     }
